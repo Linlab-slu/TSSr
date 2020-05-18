f6bcace7dad3b41af4dae94528df48482ccb41f5
zhaolianlu <luzhaolian@gmail.com>
2020-05-17 23:27:42 -0500

    fix example error

---
+++ R/example.R
@@ -0,0 +1,15 @@
+#' TSSr example data
+#'
+#' Subset of the CAGE dataset from the paper
+#' Lu and Lin, Genome Research 2019 Jul;29(7):1198-1210.
+#'
+#' @format Example data from various stages of CAGEfightR:
+#' \describe{
+#'   \item{exampleTSSr}{RangedSummarizedExperiment:TSSr}}
+#' @source \url{https://genome.cshlp.org/content/29/7/1198.short}
+#' @examples
+#' data(exampleTSSr)
+"exampleTSSr"
+
+#' @rdname exampleTSSr
+"exampleTSSr"
