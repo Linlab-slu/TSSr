2b1335dd5bba250c6d35716863895bb2c4bef748
YUZHAN2019 <46735110+YUZHAN2019@users.noreply.github.com>
2021-07-19 12:02:40 -0500

    Update ExpressionMethods.R

--- R/ExpressionMethods.R
+++ R/ExpressionMethods.R
@@ -18,10 +18,10 @@
 #' \donttest{
 #' deGene(exampleTSSr,comparePairs=list(c("control","treat")), pval = 0.01)
 #' }
-setGeneric("deGene",function(object, comparePairs=list(c("control","treat")), pval=0.01)standardGeneric("deGene"))
+setGeneric("deGene",function(object, comparePairs=list(c("control","treat")), pval=0.01,useMultiCore=FALSE, numCores = NULL)standardGeneric("deGene"))
 #' @rdname deGene
 #' @export
-setMethod("deGene",signature(object = "TSSr"), function(object, comparePairs, pval){
+setMethod("deGene",signature(object = "TSSr"), function(object, comparePairs, pval, useMultiCore, numCores){
   ##initialize data
   message("\nCalculating gene differential expression...")
   objName <- deparse(substitute(object))
