f496faa6ddc7f4d26f3c3b472381775c2f3ccd82
YUZHAN2019 <46735110+YUZHAN2019@users.noreply.github.com>
2021-07-19 00:54:58 -0500

    Update ExpressionMethods.R
    updated deGene using raw count data from gene_raw_count function

--- R/ExpressionMethods.R
+++ R/ExpressionMethods.R
@@ -18,42 +18,61 @@
 #' \donttest{
 #' deGene(exampleTSSr,comparePairs=list(c("control","treat")), pval = 0.01)
 #' }
-setGeneric("deGene",function(object, comparePairs=list(c("control","treat")), pval=0.01, useMultiCore=FALSE, numCores = NULL)standardGeneric("deGene"))
+setGeneric("deGene",function(object, comparePairs=list(c("control","treat")), pval=0.01)standardGeneric("deGene"))
 #' @rdname deGene
 #' @export
-setMethod("deGene",signature(object = "TSSr"), function(object, comparePairs, pval, useMultiCore, numCores){
+setMethod("deGene",signature(object = "TSSr"), function(object, comparePairs, pval){
   ##initialize data
   message("\nCalculating gene differential expression...")
   objName <- deparse(substitute(object))
   sampleLabels <- object@sampleLabels
   sampleLabelsMerged <- object@sampleLabelsMerged
+  gene_raw_count <- object@TAGtables
   ##define variable as a NULL value
   padj = NULL
-
-  D <- lapply(as.list(seq(comparePairs)), function(i){
-    sampleOne <- comparePairs[[i]][1]
-    sampleTwo <- comparePairs[[i]][2]
-    cx <- object@assignedClusters[[sampleOne]]
-    cy <- object@assignedClusters[[sampleTwo]]
-    tss.raw <- object@TSSrawMatrix
-    mergeIndex <- object@mergeIndex
-    samplex <- sampleLabels[which(mergeIndex ==which(sampleLabelsMerged == sampleOne))]
-    sampley <- sampleLabels[which(mergeIndex ==which(sampleLabelsMerged == sampleTwo))]
-    DE.dt <- .deseq2(object,cx,cy,tss.raw,samplex, sampley, sampleOne, sampleTwo,useMultiCore, numCores)
-    DE.sig <- subset(DE.dt, padj < pval)
-    DE.dt$gene <- row.names(DE.dt)
-    DE.sig$gene <- row.names(DE.sig)
-    DE.dt <- DE.dt[,c(ncol(DE.dt), 1:(ncol(DE.dt)-1))]
-    DE.sig <- DE.sig[,c(ncol(DE.sig), 1:(ncol(DE.sig)-1))]
-    setDT(DE.dt)
-    setDT(DE.sig)
-    DE <- list("DEtable" = DE.dt, "DEsig" = DE.sig)
-    return(DE)
-  })
-  D.names <- sapply(as.list(seq(comparePairs)), function(i){
-    paste0(comparePairs[[i]][1],"_VS_",comparePairs[[i]][2], sep ="")
-  })
-  names(D) <- D.names
-  object@DEtables <- D
-  assign(objName, object, envir = parent.frame())
+  D_updated <- lapply(as.list(seq(comparePairs)), function(i){
+  sampleOne <- comparePairs[[i]][1]
+  samplex1 <- sampleLabels[which(mergeIndex ==which(sampleLabelsMerged == sampleOne))]
+  sampleTwo <- comparePairs[[i]][2]
+  sampley1 <- sampleLabels[which(mergeIndex ==which(sampleLabelsMerged == sampleTwo))]
+  #different with expression by Deseq
+  ##get raw count tables
+  one <- gene_raw_count[[sampleOne]]
+  two <- gene_raw_count[[sampleTwo]]
+  ##merge the two raw count tables together by genes
+  one[,2:ncol(one)] <- sapply(one[,2:ncol(one)], as.integer)
+  two[,2:ncol(two)] <- sapply(two[,2:ncol(two)], as.integer)
+  #setnames(one, colnames(one), c("gene",samplex))
+  #setnames(two, colnames(two), c("gene",sampley))
+  Dtable <- merge(one, two, by = c("gene"), all = T)
+  Dtable[is.na(Dtable)] = 0
+  rownames(Dtable) <- Dtable[,1]
+  Dtable1 <- Dtable[,-1]
+  Dtable1 <- data.matrix(Dtable1)
+  #default two replicates
+  condition <- factor(c(rep(sampleOne, times = length(samplex1)), rep(sampleTwo, times = length(sampley1))))
+  dds <- DESeqDataSetFromMatrix(countData = Dtable1,data.frame(condition), ~ condition)
+  dds$condition <- factor(dds$condition, levels = c(sampleOne, sampleTwo))
+  dds <- DESeq(dds)
+  res <- results(dds)
+  res1 <- as.data.frame(res)
+  res1$gene <- rownames(res1)
+  res2 <- merge(Dtable,res1,by=c("gene"),all=T)
+  res2 <- res2[order(res2$padj),]
+  DE.dt <- as.data.frame(res2)
+  DE.sig <- subset(DE.dt, padj < pval)
+  #DE.dt <- DE.dt[,c(ncol(DE.dt), 1:(ncol(DE.dt)-1))]
+  #DE.sig <- DE.sig[,c(ncol(DE.sig), 1:(ncol(DE.sig)-1))]
+  setDT(DE.dt)
+  setDT(DE.sig)
+  DE <- list("DEtable" = DE.dt, "DEsig" = DE.sig)
+  return(DE)
+})
+D.names <- sapply(as.list(seq(comparePairs)), function(i){
+  paste0(comparePairs[[i]][1],"_VS_",comparePairs[[i]][2], sep ="")
+})
+names(D_updated) <- D.names
+return(D_updated)
+object@DEtables <- D_updated
+assign(objName, object, envir = parent.frame())
 })
