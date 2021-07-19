36690e4817011ef7f2186b14b3ee71eb08a4e612
YUZHAN2019 <46735110+YUZHAN2019@users.noreply.github.com>
2021-07-19 17:52:13 -0500

    Update ExpressionFunctions.R

--- R/ExpressionFunctions.R
+++ R/ExpressionFunctions.R
@@ -129,40 +129,85 @@
   return(cs)
 }
 ############################################################################
-#tagcounts for all genes for all samples
-genes_counts_for_DESeq2 <- function(object,useMultiCore,numCores){
-gene_raw_count <- lapply(as.list(seq(sampleLabelsMerged)), function(i){
-  cx <- object@assignedClusters[[i]]
-  tss.raw <- object@TSSrawMatrix
-  mergeIndex <- object@mergeIndex
-  sampleOne <- sampleLabelsMerged[i]
-  samplex <- sampleLabels[which(mergeIndex ==which(sampleLabelsMerged == sampleOne))]
-  xCounts <-.tagCount_updated(cx, tss.raw,samplex,useMultiCore, numCores)
+.deseq2_test <- function(object,cx,cy, tss.raw, samplex,sampley, sampleOne,sampleTwo,useMultiCore, numCores){
+  ##get raw count tables
+  load("TAGtable_temp.RData")
+  #sampleX <- sampleLabels[which(mergeIndex ==which(sampleLabelsMerged == names(object@TAGtables)))]
+  #sampleY <- sampleLabels[which(mergeIndex ==which(sampleLabelsMerged == names(object@TAGtables)))]
+  if(sampleOne%in%names(TAGtables)){
+    xCounts<-TAGtables[[which(names(TAGtables)==sampleOne)]]
+  } else{
+    xCounts <-.tagCount_updated(cx, tss.raw,samplex,useMultiCore, numCores)
+    ## save the tagCount results
+    TAGtables[[sampleOne]]<-xCounts
+  }
+  
+  if(sampleTwo%in%names(TAGtables)){
+    yCounts<-TAGtables[[which(names(TAGtables)==sampleTwo)]]
+  } else{
+    yCounts <-.tagCount_updated(cy, tss.raw,sampley,useMultiCore, numCores)
+    TAGtables[[sampleTwo]]<-yCounts
+  }
+  #save TAGtable as temp file
+  save(TAGtables, file="TAGtable_temp.RData")
   xCounts <- xCounts[,-c(2:11)]
+  yCounts <- yCounts[,-c(2:11)]
   ##tag counts by gene for sampleOne
   setkey(xCounts, gene)
   if(useMultiCore){
     if(is.null(numCores)){
       numCores <- detectCores()
     }
-    one_temp <- mclapply(as.list(unique(xCounts$gene)), function(my.gene) {
+    one <- mclapply(as.list(unique(xCounts$gene)), function(my.gene) {
       data <- xCounts[list(my.gene)]
       return(c(my.gene,colSums(data[,-c(1,2,3)])))
     }, mc.cores = numCores)
   }else{
-    one_temp <- lapply(as.list(unique(xCounts$gene)), function(my.gene) {
+    one <- lapply(as.list(unique(xCounts$gene)), function(my.gene) {
       data <- xCounts[list(my.gene)]
       return(c(my.gene,colSums(data[,-c(1,2,3)])))
     })
   }
-  one_temp <- data.frame(matrix(unlist(one_temp), nrow=length(one_temp), byrow=T),stringsAsFactors=FALSE)
-  one_temp <- as.data.frame(one_temp)
-  colnames(one_temp) <- c("gene",samplex)
-  return(one_temp)
-})
-names(gene_raw_count) <- sampleLabelsMerged
-object@TAGtables <- gene_raw_count
+  one <- data.frame(matrix(unlist(one), nrow=length(one), byrow=T),stringsAsFactors=FALSE)
+  
+  ##tag counts by gene for sampleTwo
+  setkey(yCounts, gene)
+  if(useMultiCore){
+    if(is.null(numCores)){
+      numCores <- detectCores()
+    }
+    two <- mclapply(as.list(unique(yCounts$gene)), function(my.gene) {
+      data <- yCounts[list(my.gene)]
+      return(c(my.gene,colSums(data[,-c(1,2,3)])))
+    }, mc.cores = numCores)
+    
+  }else{
+    two <- lapply(as.list(unique(yCounts$gene)), function(my.gene) {
+      data <- yCounts[list(my.gene)]
+      return(c(my.gene,colSums(data[,-c(1,2,3)])))
+    })
+  }
+  two <- data.frame(matrix(unlist(two), nrow=length(two), byrow=T),stringsAsFactors=FALSE)
+  ##merge the two raw count tables together by genes
+  one[,2:ncol(one)] <- sapply(one[,2:ncol(one)], as.integer)
+  two[,2:ncol(two)] <- sapply(two[,2:ncol(two)], as.integer)
+  setnames(one, colnames(one), c("gene",samplex))
+  setnames(two, colnames(two), c("gene",sampley))
+  Dtable <- merge(one, two, by = c("gene"), all = T)
+  Dtable[is.na(Dtable)] = 0
+  ##
+  rownames(Dtable) <- Dtable[,1]
+  Dtable <- Dtable[,-1]
+  Dtable <- data.matrix(Dtable)
+  condition <- factor(c(rep(sampleOne, times = length(samplex)), rep(sampleTwo, times = length(sampley))))
+  dds <- DESeqDataSetFromMatrix(countData = Dtable,data.frame(condition), ~ condition)
+  dds$condition <- factor(dds$condition, levels = c(sampleOne, sampleTwo))
+  dds <- DESeq(dds)
+  res <- results(dds)
+  res <- res[order(res$padj),]
+  return(as.data.frame(res))
 }
+
 ############################################################################
 ##.tagCount updated
 .tagCount_updated <- function(cs, tss.raw, samples, useMultiCore, numCores){
