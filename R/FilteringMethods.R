2ede23950f471a78e25f20b844b61a675c032ef2
Zhenbin <zhenbinhu@gmail.com>
2021-07-21 17:43:51 -0500

    Update FilteringMethods.R

--- R/FilteringMethods.R
+++ R/FilteringMethods.R
@@ -35,7 +35,7 @@ setMethod("filterTSS",signature(object = "TSSr"), function(object, method, norma
   for (chrom in 1:length(Genome)) {
     genomeSize <- genomeSize + length(Genome[[chrom]])
   }
-  tss.dt <- object@TSSrawMatrix
+  tss.dt <- object@TSSprocessedMatrix
   ##define variable as a NULL value
   tags = NULL
 
