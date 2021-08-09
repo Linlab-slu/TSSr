aa4aecb25b95a283b8a08310025635e38cd3ed54
zhaolianlu <luzhaolian@gmail.com>
2021-08-09 13:41:25 +0800

    update vignette

--- doc/vignette.R
+++ doc/vignette.R
@@ -1,60 +1,74 @@
+## ----setup, include = FALSE---------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
 
-## ----eval=TRUE----------------------------------------------------------------
+## ----github, eval=FALSE-------------------------------------------------------
+#  devtools::install_github("Linlab-slu/TSSr")
+
+## ----library, results='hide', message=FALSE-----------------------------------
 library(TSSr)
 
-## ----eval=TRUE----------------------------------------------------------------
-inputFiles <- c("S01.sorted.bam", "S02.sorted.bam", "S03.sorted.bam", "S04.sorted.bam")
-myTSSr <- new("TSSr", genomeName = "BSgenome.Scerevisiae.UCSC.sacCer3"
-              ,inputFiles = inputFiles
-              ,inputFilesType= "bam"
-              ,sampleLabels = c("SL01","SL02","SL03","SL04")
-              ,sampleLabelsMerged = c("control","treat")
-              ,refSource = "saccharomyces_cerevisiae.SGD.gff"
-              ,organismName = "saccharomyces cerevisiae")
+## ----citation, eval=TRUE------------------------------------------------------
+citation("TSSr")
 
-## ----eval=TRUE----------------------------------------------------------------
-getTSS(myTSSr)
-plotCorrelation(myTSSr, samples = "all")
-plotTssPCA(myTSSr, TSS.threshold=10)
-mergeSamples(myTSSr,mergeIndex=c(1,1,2,2))
+## ----exampleTSSr, results="hide", tidy=FALSE----------------------------------
+# Load the example data
+data("exampleTSSr")
+myTSSr <- exampleTSSr
+
+## ----newTSSr, eval=FALSE------------------------------------------------------
+#  # Provide bam files
+#  inputFiles <- c("S01.sorted.bam", "S02.sorted.bam", "S03.sorted.bam", "S04.sorted.bam")
+#  myTSSr <- new("TSSr", genomeName = "BSgenome.Scerevisiae.UCSC.sacCer3"
+#            ,inputFiles = inputFiles
+#            ,inputFilesType= "bam"
+#            ,sampleLabels = c("SL01","SL02","SL03","SL04")
+#            ,sampleLabelsMerged = c("control","treat")
+#            ,mergeIndex = c(1,1,2,2)
+#            ,refSource = "saccharomyces_cerevisiae.SGD.gff"
+#            ,organismName = "saccharomyces cerevisiae")
+#  myTSSr
+
+## ----getTSS, eval=FALSE-------------------------------------------------------
+#  # Get TSS
+#  getTSS(myTSSr)
+
+## ----TSSprocessing, tidy=FALSE------------------------------------------------
+# Merge replicates
+mergeSamples(myTSSr)
+# Normalization
 normalizeTSS(myTSSr)
-filterTSS(myTSSr, method = "TPM",tpmLow=0.1)
+# TSS filtering
+filterTSS(myTSSr, method = "TPM", tpmLow = 0.1)
 
-## ----eval=TRUE----------------------------------------------------------------
-exportTSStable(myTSSr, data = "raw", merged = "TRUE")
-exportTSStoBedgraph(myTSSr, data = "processed", format = "bedGraph")
-exportTSStoBedgraph(myTSSr, data = "processed", format = "BigWig")
+## ----TSSclustering, eval=FALSE------------------------------------------------
+#  # TSS clustering
+#  clusterTSS(myTSSr, method = "peakclu",peakDistance=100,extensionDistance=30
+#           ,localThreshold = 0.02,clusterThreshold = 1
+#           ,useMultiCore=FALSE, numCores=NULL)
+#  
+#  # Aggregating consensus clusters
+#  consensusCluster(myTSSr, dis = 50, useMultiCore = FALSE)
 
-## ----eval=TRUE----------------------------------------------------------------
-clusterTSS(myTSSr, method = "peakclu",peakDistance=100,extensionDistance=30
-           ,localThreshold = 0.02,clusterThreshold = 1
-           ,useMultiCore=FALSE, numCores=NULL)
-exportClustersTable(myTSSr, data = "filtered")
-exportClustersToBed(myTSSr, data = "tagClusters")
+## ----shapeCluster, eval=FALSE-------------------------------------------------
+#  # Calculating core promoter shape score
+#  shapeCluster(myTSSr,clusters = "consensusClusters", method = "PSS",
+#               useMultiCore= FALSE, numCores = NULL)
 
-## ----eval=TRUE----------------------------------------------------------------
-consensusCluster(myTSSr, dis = 50, useMultiCore = FALSE)
-exportClustersToBed(myTSSr, data = "consensusClusters")
-
-## ----eval=TRUE----------------------------------------------------------------
-shapeCluster(myTSSr,clusters = "consensusClusters", method = "PSS",useMultiCore= FALSE, numCores = NULL)
-plotInterQuantile(myTSSr,samples = "all",tagsThreshold = 1)
-
-## ----eval=TRUE----------------------------------------------------------------
-exportShapeTable(myTSSr)
-
-## ----eval=TRUE----------------------------------------------------------------
+## ----annotateCluster, tidy=FALSE----------------------------------------------
+# Assign clusters to the annotated features
 annotateCluster(myTSSr,clusters = "consensusClusters",filterCluster = TRUE,
-                filterClusterThreshold = 0.02, annotationType = "genes"
-                ,upstream=1000, upstreamOverlap = 500, downstream = 0)
+              filterClusterThreshold = 0.02, annotationType = "genes"
+              ,upstream=1000, upstreamOverlap = 500, downstream = 0)
 
-## ----eval=TRUE----------------------------------------------------------------
-deGene(myTSSr,comparePairs=list(c("control","treat")), pval = 0.01,useMultiCore=FALSE, numCores=NULL)
-exportShiftTable(myTSSr)
-plotDE(myTSSr, withGeneName = "TRUE",xlim=c(-2.5, 2.5),ylim=c(0,10))
-exportDETable(myTSSr, data = "sig")
+## ----deGEne, tidy=FALSE-------------------------------------------------------
+# Assign clusters to the annotated features
+deGene(myTSSr,comparePairs=list(c("control","treat")), 
+       pval = 0.01,useMultiCore=FALSE, numCores=NULL)
 
-## ----eval=TRUE----------------------------------------------------------------
+## ----shiftPromoter, tidy=FALSE------------------------------------------------
+# Calcuate core promoter shifts
 shiftPromoter(myTSSr,comparePairs=list(c("control","treat")), pval = 0.01)
-plotTSS(myTSSr,samples=c("control","treat"),tssData = "processed",clusters = "filtered",clusterThreshold = 0.02
-        ,genelist=c("YBL017C","YBL067C"),up.dis =500,down.dis = 100)
+
